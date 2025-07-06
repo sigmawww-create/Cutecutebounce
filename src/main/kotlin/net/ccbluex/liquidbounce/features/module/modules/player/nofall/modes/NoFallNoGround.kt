5a3495b6c5f72697e7bad53d584315dfedf65126
sigmawww-create <sigmamanalpahfrfr@gmail.com>
2025-07-06 15:40:31 -0600

    NoFallNoGroundIDONTTHINKTHISBYPASSESMUCHANYMORE.kt

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/nofall/modes/NoFallNoGround.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/nofall/modes/NoFallNoGround.kt
@@ -2,7 +2,7 @@
  * This file is part of LiquidBounce (https://github.com/CCBlueX/LiquidBounce)
  *
  * Copyright (c) 2015 - 2025 CCBlueX
- *
+ *e
  * LiquidBounce is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
